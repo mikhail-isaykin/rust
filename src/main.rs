be457e1f3d00fe33aa3fd098f66ddbe28b6e5358
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-16 20:42:36 +0300

    feat: sum of first and last digits of a number

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let num: i32 = 123;
     let text: String = num.to_string();
-
-    println!("{}", text.chars().next().unwrap());
+    let first: i32 = text.chars().next().unwrap().to_digit(10).unwrap() as i32;
+    let last: i32 = text.chars().last().unwrap().to_digit(10).unwrap() as i32;
+    println!("{}", first + last);
 }
