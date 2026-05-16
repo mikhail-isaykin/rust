ff5a3b97548f7d010fe0a98c30db3361608b3062
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-17 01:33:05 +0300

    feat: check if second digit is greater than first

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 fn main() {
-    let num: u8 = 30;
-    
-    match num {
-        1..=15 => println!("{}", 1),
-        16..=30 => println!("{}", 2),
-        31..=45 => println!("{}", 3),
-        46..=60 => println!("{}", 4),
-        _ => println!("{}", { false }),
+    let num: u8 = 22;
+    let txt: String = num.to_string();
 
-    }
+    let first: u32 = txt.chars().next().unwrap().to_digit(10).unwrap();
+    let last: u32 = txt.chars().last().unwrap().to_digit(10).unwrap();
+
+    println!("{}", last > first);
 }
