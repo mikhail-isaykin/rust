2400d507261a06b60ee0eb6ff65929c70c0fad68
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-17 00:25:53 +0300

    feat: parse string to integer

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
 fn main() {
-    let num1: u16 = 142;
-    let num2: u16 = 142;
+    let txt: &str = "123";
 
-    if num1 > num2 {
-        println!("{}", num1);
-    } else if num2 > num1 {
-        println!("{}", num2);
-    } else {
-        println!("Числа равны");
-    }
+    let num: i32 = txt.parse().unwrap();
 }
