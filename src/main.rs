d135d1c9b83e9ba10921edba896d6e59b8e37676
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-17 00:59:30 +0300

    feat: print even numbers from 2 to 100

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 fn main() {
-    let num: u8 = 4;
-
-    match num {
-        1..=3 => println!("Winter"),
-        4..=6 => println!("Spring"),
-        7..=9 => println!("Summer"),
-        10..=12 => println!("Autumn"),
-        _ => println!("{}", { false }),
+    for num in 1..=100 {
+        if num % 2 == 0 {
+            println!("{}", num);
+        }
     }
 }
