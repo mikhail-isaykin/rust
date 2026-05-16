2f3aba72c00aa8003a5b2a40e63aabbc3df54fa1
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-16 21:14:33 +0300

    feat: check if number is even or odd

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
 fn main() {
-    let num: i32 = 123;
-    let text: String = num.to_string();
-    let first: i32 = text.chars().next().unwrap().to_digit(10).unwrap() as i32;
-    let last: i32 = text.chars().last().unwrap().to_digit(10).unwrap() as i32;
-    println!("{}", first + last);
+    let num: i32 = 1234;
+    println!("{}", if num % 2 == 0 { "Четное" } else { "Нечетное" });
 }
