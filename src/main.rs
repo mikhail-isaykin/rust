08562cffcff3f8042e05eae8d0df3986af719f8e
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-17 01:46:10 +0300

    feat: check if first digits of two numbers are equal

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
-    for num in -100..=100 {
-        if num % 7 == 0 {
-            println!("{}", num);
-        }
-    }
+    let num1: u16 = 12;
+    let num2: u16 = 13;
+
+    let first1: u16 = num1.to_string().chars().next().unwrap().to_digit(10).unwrap() as u16;
+    let first2: u16 = num2.to_string().chars().next().unwrap().to_digit(10).unwrap() as u16;
+
+    println!("{}", first1 == first2);
 }
