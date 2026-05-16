6e06081fafcc49e7b67045806790c8adc1afd7fa
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-17 00:13:52 +0300

    feat: print second to last character of string

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use std::io;
+
 fn main() {
-    for num in 1..=10 {
-        println!("{}", num);
+    let mut text: String = String::new();
+
+    io::stdin()
+        .read_line(&mut text)
+        .unwrap();
+
+    let len_text: usize = text.trim().chars().count();
+
+    if len_text > 1 {
+        println!("{}", text.trim().chars().nth(len_text - 2).unwrap());
+    } else {
+        println!("{}", { false });
     }
 }
