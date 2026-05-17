4b2b4f2f60698baa68e79d728c03fb1f12c6a5c0
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-17 22:00:20 +0300

    feat: form unique team from player nicknames using HashSet

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 use std::collections::HashSet;
 use std::io;
 
-fn main() {
-    let allowed_employees: HashSet<i32> = HashSet::from([123, 456, 789, 101, 202]);
-
-
-    let mut employee_id = String::new();
-
+fn write(s: &mut String) -> () {
     io::stdin()
-        .read_line(&mut employee_id)
+        .read_line(s)
         .unwrap();
+}
 
-    let employee_id: i32 = employee_id.trim().parse().unwrap();
+fn main() {
+    let mut team: HashSet<String> = HashSet::new();
+    let mut num_players = String::new();
+    
+    write(&mut num_players);
+    
+    let num_players: u32 = num_players.trim().parse().unwrap();
 
-    println!("{}", allowed_employees.contains(&employee_id));
+    for _ in 0..num_players {
+        let mut player: String = String::new();
+        write(&mut player);
+        team.insert(player.trim().to_string());
+    }
+
+    println!("Количество: {:?}", team.len());
 
 }
