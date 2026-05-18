b40e92f68053fb459d3f28e03920ba8067477d8f
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-19 02:28:29 +0300

    feat: solve rust bytes to kb mb gb conversion

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 fn main() {
-    let txt: &str = "abcde";
+    let mut current: f64 = 7435421243.0;
 
-    for chr in txt.chars().rev() {
-        println!("{}", chr);
+    let units: [&str; 4] = ["byte", "kb", "mb", "gb"];
+
+    println!("{:.3}: {}", current, units[0]);
+
+    for i in 1..=3 {
+        current /= 1024.0;
+        println!("{:.3}: {}", current, units[i]);
     }
 }
