1dd99e73c4d2be17aaa80039e4332d930ec62497
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-18 23:36:01 +0300

    23:35feat: solve rust kilobytes to bytes conversion

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
 fn main() {
-    let chr1: char = 'a';
-    let chr2: char = 'b';
-    let chr3: char = 'c';
+    let kb: u32 = 35;
+    let byte: u32 = kb * 1024;
 
-    let mut txt: String = chr1.to_string();
-
-    txt.push(chr2);
-    txt.push(chr3);
-
-    println!("{}", txt);
+    println!("{}", byte);
 }
