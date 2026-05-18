1823404aee10fc496051b3b3107655e35a18f196
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-19 01:55:49 +0300

    feat: solve rust countdown from 100 to 1

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 fn main() {
-    let gb: f32 = 35.24;
-    let mb: f32 = gb * 1024.0;
-    let kb: f32 = mb * 1024.0;
-    let byte: f32 = kb * 1024.0;
-
-    println!("{:?}", [gb, mb, kb, byte])
+    for num in (1..=100).rev() {
+        println!("{}", num);
+    }
 }
