5b0938782e6060228dd1f3e23213cf05013f0085
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-18 23:48:53 +0300

    feat: solve rust sum from 1 to 100

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 fn main() {
-    let num: u16 = 12;
+    let mut sum: u32 = 0;
 
-    match num {
-        1..=10 => println!("{}", 1),
-        11..=20 => println!("{}", 2),
-        21..=31 => println!("{}", 3),
-        _ => println!("invalid day"),
+    for num in 1..=100 {
+        sum += num;
     }
+    println!("{}", sum);
 }
