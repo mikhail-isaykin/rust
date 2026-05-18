fd043c3b4131755d08a8cb204041008366cce18c
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-18 22:53:52 +0300

    feat: solve rust divisibility task

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,6 @@
-use std::collections::HashSet;
-use std::io;
-
-fn write(s: &mut String) -> () {
-    io::stdin()
-        .read_line(s)
-        .unwrap();
-}
-
 fn main() {
-    let mut team: HashSet<String> = HashSet::new();
-    let mut num_players = String::new();
-    
-    write(&mut num_players);
-    
-    let num_players: u32 = num_players.trim().parse().unwrap();
+    let num1: u16 = 36;
+    let num2: u16 = 12;
 
-    for _ in 0..num_players {
-        let mut player: String = String::new();
-        write(&mut player);
-        team.insert(player.trim().to_string());
-    }
-
-    println!("Количество: {:?}", team.len());
-
+    println!("{}", num1 % num2 == 0);
 }
