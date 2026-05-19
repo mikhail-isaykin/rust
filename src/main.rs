3febab94546c58dec2a5c66981c91a3a8d41f7dd
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-19 23:30:50 +0300

    feat: calculate 10 percent of a number

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 fn main() {
-    let num: u32 = 1234567;
-    let txt: String = num.to_string();
+    let num: f32 = 12.3;
 
-    println!("{:?}", txt.chars().filter_map(|s| s.to_digit(10)).sum::<u32>());
+    println!("{}", num * 0.1);
 }
