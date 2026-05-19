985a7db0b4f4d7e7877e56a51a9d7ef8eb7f2799
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-20 01:19:14 +0300

    solve: seconds in 10 days

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,8 @@
-use std::io;
-
-fn write (word: &mut String) -> () {
-    io::stdin()
-        .read_line(word)
-        .unwrap();
-}
-
-fn chr (word: &str, flag: &str) -> char {
-    if flag == "first" {
-        word.trim().chars().last().unwrap()
-    } else if flag == "last" {
-        word.trim().chars().next().unwrap()
-    } else {
-        panic!("unknown flag")
-    }
-}
 fn main() {
-    let mut word1: String = String::new();
-    let mut word2: String = String::new();
+    let days: u32 = 10;
+    let hours: u32 = days * 24;
+    let minutes: u32 = hours * 60;
+    let seconds: u32 = minutes * 60;
 
-    write(&mut word1);
-    write(&mut word2);
-
-    println!("{}", chr(&word1, "first") == chr(&word2, "last"));
-
+    println!("{}", seconds);
 }
