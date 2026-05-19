30d15e944d0c0061247c89f424c35a3c700464c1
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-20 00:08:37 +0300

    feat: check if last letter of first word matches first letter of second word

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
-fn main() {
-    let mut sum: u32 = 0;
+use std::io;
 
-    for num in 1u32..=100 {
-        sum += num.pow(2);
+fn write (word: &mut String) -> () {
+    io::stdin()
+        .read_line(word)
+        .unwrap();
+}
+
+fn chr (word: &str, flag: &str) -> char {
+    if flag == "first" {
+        word.trim().chars().last().unwrap()
+    } else if flag == "last" {
+        word.trim().chars().next().unwrap()
+    } else {
+        panic!("unknown flag")
     }
-    
-    println!("{}", sum);
+}
+fn main() {
+    let mut word1: String = String::new();
+    let mut word2: String = String::new();
+
+    write(&mut word1);
+    write(&mut word2);
+
+    println!("{}", chr(&word1, "first") == chr(&word2, "last"));
+
 }
