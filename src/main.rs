e9022deab979757c2ea28aff97a31676893d32bb
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-20 23:22:22 +0300

    feat: print first character of each array element

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let arr: [u32; 5] = [1, 2, 3, 4, 5];
+    let arr: [&str; 3] = ["123", "456", "789"];
 
-    for num in arr {
-        println!("{}", num)
+    for txt in arr {
+        println!("{}", txt.chars().next().unwrap())
     }
 }
