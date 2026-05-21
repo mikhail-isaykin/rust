699c0dfa8f02f2db75644638669aa60cba748ef2
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-22 00:00:52 +0300

    feat: sum array elements

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
 fn main() {
-    let num: u16 = 12345;
+    let arr: [u32; 5] = [1, 2, 3, 4, 5];
 
-    let txt: String = num.to_string();
-
-    let first: u32 = txt.chars().next().unwrap().to_digit(10).unwrap();
-    let last: u32 = txt.chars().last().unwrap().to_digit(10).unwrap();
-
-    println!("{}", first + last)
+    println!("{}", arr.iter().sum::<u32>());
 }
