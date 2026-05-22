2226dab821752b6f8d1c8839193d79cdd2222c65
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-22 23:27:35 +0300

    feat: solve sum of squares of array elements

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let num: u16 = 12345;
+    let arr: [u32; 5] = [1, 2, 3, 4, 5];
 
-    for chr in num.to_string().chars().rev() {
-        println!("{}", chr);
-    }
+    let sum: u32 = arr.map(|n: u32| n.pow(2)).iter().sum();
+
+    println!("{}", sum);
 }
