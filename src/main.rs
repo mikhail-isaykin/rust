47d5a62c1e2cf0ef5bcc27baea7f3645aeda3162
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-24 23:29:47 +0300

    feat: print array elements in reverse order

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let arr: [i8; 5] = [1, 2, -3, 4, -5];
+    let arr: [u8; 5] = [1, 2, 3, 4, 5];
 
-    let sum: u8 = arr.iter().filter(|&&num| num > 0).map(|&num| num as u8).sum();
-
-    println!("{}", sum);
+    for num in arr.iter().rev() {
+        print!("{} ", num);
+    }
 }
