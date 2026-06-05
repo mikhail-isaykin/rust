0753905419f39dfefd417ca677272e688518b9ba
mikhail-isaykin <6l2i6.gh@gmail.com>
2026-06-05 23:48:44 +0300

    feat: add two-sum index search over vector

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-use std::collections::HashSet;
-
 fn main() {
-    let nums = vec![4, 1, 2, 1, 3, 4, 2, 5, 3];
+    let nums = vec![2, 7, 11, 15, 3, 6];
+    let target = 9;
 
-    let mut seen = HashSet::new();
-    let unique: Vec<i32> = nums
-        .iter()
-        .filter(|&&n| seen.insert(n))
-        .copied()
-        .collect();
-
-    println!("{:?}", unique);
+    for i in 0..nums.len() {
+        for j in (i + 1)..nums.len() {
+            if nums[i] + nums[j] == target {
+                println!("[{}, {}]", i, j);
+            }
+        }
+    }
 }
