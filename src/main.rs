bd7e2adfa27f3cc364f196c60408c6e0694ca0e9
mikhail-isaykin <6l2i6.gh@gmail.com>
2026-06-11 23:41:43 +0300

    feat: add shared ownership example using Rc

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,13 @@
-use std::collections::HashMap;
-
-fn first_unique(nums: Vec<i32>) -> Option<i32> {
-    let mut counts: HashMap<i32, usize> = HashMap::new();
-
-    // Считаем частоты
-    for num in &nums {
-        *counts.entry(*num).or_insert(0) += 1;
-    }
-
-    // Ищем первое уникальное число
-    for num in nums {
-        if counts.get(&num) == Some(&1) {
-            return Some(num);
-        }
-    }
-
-    None
-}
+use std::rc::Rc;
 
 fn main() {
-    let nums = vec![4, 2, 1, 2, 1];
+    let message = Rc::new(String::from("Hello, Rust!"));
 
-    println!("{:?}", first_unique(nums)); // Some(4)
+    let a = Rc::clone(&message);
+    let b = Rc::clone(&message);
+
+    println!("{}", a);
+    println!("{}", b);
+
+    println!("Owners: {}", Rc::strong_count(&message));
 }
